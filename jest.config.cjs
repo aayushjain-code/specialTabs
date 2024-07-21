7d9afec067c996a87f5ac6c1c8951d7490229e8b
aayushjain-code <aayushjain6020@gmail.com>
2024-07-21 23:02:04 +0530

    Integrated unit testing using jest and react-testing-library, add prettier config for standard code formatting, write test cases for ui testing

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.jest.json",
+      },
+    ],
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+};
